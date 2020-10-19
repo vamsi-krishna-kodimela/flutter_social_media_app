01f53d40f44068db6045b71e85748f9e908e248c
Vamsi <vamsikrishnakdml@gmail.com>
2020-10-19 08:16:40 +0530

    entertainment_Screen.dart created

---
+++ lib/screens/entertainment_screen/entertainment_Screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EntertainmentScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: ListView.builder(itemBuilder: null),
+    );
+  }
+}
