95c0b721b986712206e72f9c7eebe91df9b3320c
Vamsi <vamsikrishnakdml@gmail.com>
2020-10-26 15:18:43 +0530

    Added Group members tile ui

---
+++ lib/screens/single_group_screen/components/group_members_component.dart
@@ -0,0 +1,137 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:social_media/constants.dart';
+import 'package:social_media/screens/single_user_screen/single_user_screen.dart';
+
+class GroupMembersComponent extends StatelessWidget {
+  GroupMembersComponent({
+    Key key,
+    @required this.members,
+    @required this.admins,
+  }) : super(key: key);
+
+  final List members;
+  final List admins;
+  final String uid = FirebaseAuth.instance.currentUser.uid;
+
+  @override
+  Widget build(BuildContext context) {
+    // members.removeWhere((element) => element==uid);
+    return ListView.builder(
+      itemCount: members.length,
+      itemBuilder: (ctx, i) {
+        final _firestore =
+            FirebaseFirestore.instance.collection("users").doc(members[i]);
+        return StreamBuilder<DocumentSnapshot>(
+          stream: _firestore.snapshots(),
+          builder: (_, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting)
+              return Shimmer.fromColors(
+                child: ListTile(
+                  title: Text("Loading..."),
+                ),
+                baseColor: Colors.grey[300],
+                highlightColor: Colors.grey[100],
+              );
+            final _userinfo = snapshot.data.data();
+            if (!admins.contains(uid))
+              return Card(
+                key: Key(members[i]),
+                elevation: 0.0,
+                child: ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) =>
+                            SingleUserScreen(members[i], _userinfo["name"]),
+                      ),
+                    );
+                  },
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                      8.0,
+                    ),
+                    child: AspectRatio(
+                      aspectRatio: 1.0,
+                      child: FancyShimmerImage(
+                        imageUrl: _userinfo["photoUrl"],
+                        boxFit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    _userinfo["name"],
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  subtitle:
+                      Text(admins.contains(members[i]) ? "Admin" : "Member"),
+                ),
+              );
+
+            return Dismissible(
+              direction: DismissDirection.endToStart,
+              key: Key(members[i]),
+              background: Container(
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.only(right: kDefaultPadding),
+                color: kAccentColor,
+                child: Icon(
+                  Icons.delete,
+                  color: kWhite,
+                ),
+              ),
+              child: Card(
+                elevation: 0.0,
+                child: ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) =>
+                            SingleUserScreen(members[i], _userinfo["name"]),
+                      ),
+                    );
+                  },
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                      8.0,
+                    ),
+                    child: AspectRatio(
+                      aspectRatio: 1.0,
+                      child: FancyShimmerImage(
+                        imageUrl: _userinfo["photoUrl"],
+                        boxFit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    _userinfo["name"],
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  subtitle:
+                      Text(admins.contains(members[i]) ? "Admin" : "Member"),
+                  trailing: IconButton(
+                    onPressed: (){},
+                    icon: Icon(
+                      Icons.admin_panel_settings,
+                      color:
+                          admins.contains(members[i]) ? kAccentColor : kGreen,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
--- lib/screens/single_group_screen/single_group_screen.dart
+++ lib/screens/single_group_screen/single_group_screen.dart
@@ -1,10 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
 import 'package:social_media/constants.dart';
-import 'components/group_description_component.dart';
 import 'components/group_info_component.dart';
+import 'components/group_members_component.dart';
 import 'components/group_tab_option_component.dart';
 
 class SingleGroupScreen extends StatefulWidget {
@@ -20,7 +18,6 @@ class SingleGroupScreen extends StatefulWidget {
 class _SingleGroupScreenState extends State<SingleGroupScreen> {
   final uid = FirebaseAuth.instance.currentUser.uid;
   int option = 0;
-
   void optionChanger(val) {
     setState(() {
       option = val;
@@ -30,7 +27,8 @@ class _SingleGroupScreenState extends State<SingleGroupScreen> {
   @override
   Widget build(BuildContext context) {
     final List<dynamic> members = widget.gData["members"];
-    print(members);
+    // members.removeWhere((element) => element==uid);
+    final List<dynamic> admins = widget.gData["admins"];
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
@@ -67,30 +65,9 @@ class _SingleGroupScreenState extends State<SingleGroupScreen> {
               ],
             ),
           ),
+          SizedBox(height: kDefaultPadding,),
           Expanded(
-            child: ListView.builder(
-              itemCount: members.length,
-              itemBuilder: (ctx, i) {
-                final _firestore = FirebaseFirestore.instance
-                    .collection("users")
-                    .doc(members[i]);
-                return StreamBuilder<DocumentSnapshot>(
-                  stream: _firestore.snapshots(),
-                  builder: (_, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting)
-                      return Shimmer.fromColors(
-                        child: ListTile(
-                          title: Text("Loading..."),
-                        ),
-                        baseColor: Colors.grey[300],
-                        highlightColor: Colors.grey[100],
-                      );
-                    final _userinfo = snapshot.data.data();
-                    return ListTile(title: Text(_userinfo["name"]),);
-                  },
-                );
-              },
-            ),
+            child: GroupMembersComponent(members: members, admins: admins),
           ),
         ],
       ),
